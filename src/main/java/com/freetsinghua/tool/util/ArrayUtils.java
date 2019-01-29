1931f6be7cef4ea54a9681983945ef299bd1f2de
tsinghua <zqh198971@163.com>
2019-01-29 20:15:53 +0800

    添加创建数组方法

--- src/main/java/com/freetsinghua/tool/util/ArrayUtils.java
+++ src/main/java/com/freetsinghua/tool/util/ArrayUtils.java
@@ -1,5 +1,6 @@
 package com.freetsinghua.tool.util;
 
+import java.lang.reflect.Array;
 import java.util.Collections;
 
 /**
@@ -8,5 +9,16 @@ import java.util.Collections;
 public class ArrayUtils {
     
     public static final Object[] EMPTY_ARRAY = Collections.EMPTY_LIST.toArray();
-    
+
+    /**
+     * 创建数组
+     * @param clazz 类型
+     * @param length 数组长度
+     * @param <T> 数组类型
+     * @return 返回数组
+     */
+    @SuppressWarnings("unchecked")
+    public static <T> T[] newArray(Class<T> clazz, int length) {
+        return (T[]) Array.newInstance(clazz, length);
+    }
 }
