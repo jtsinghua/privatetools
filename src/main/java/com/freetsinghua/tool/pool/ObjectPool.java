293eba8ff2c48ed1a03c310a9bc0f2a7aa30cb58
tsinghua <zqh198971@163.com>
2019-02-02 20:27:14 +0800

    添加

---
+++ src/main/java/com/freetsinghua/tool/pool/ObjectPool.java
@@ -0,0 +1,10 @@
+package com.freetsinghua.tool.pool;
+
+import org.apache.commons.pool2.impl.GenericObjectPool;
+
+/**
+ * @author z.tsinghua
+ * @date 2019/2/2
+ */
+public class ObjectPool<T> {
+}
